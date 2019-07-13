30640fe7db3d9ede149b9fd7afdd348d02fd815a
Eric Barb <eric.barb@gmail.com>
2019-07-12 23:14:39 -0400

    checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,1091 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'PeriodicTable2';
+  title = "PeriodicTable2";
+  elements = [
+    {
+      name: "Hydrogen",
+      symbol: "H",
+      r: 1,
+      c: 1,
+      type: "NonMetal",
+      number: 1,
+      weight: 1.008
+    },
+    {
+      name: "Helium",
+      symbol: "He",
+      r: 1,
+      c: 18,
+      type: "NobleGas",
+      number: 2,
+      weight: 4.003
+    },
+    {
+      name: "Lithium",
+      symbol: "Li",
+      r: 2,
+      c: 1,
+      type: "AlkaliMetal",
+      number: 3,
+      weight: 6.941
+    },
+    {
+      name: "Beryllium",
+      symbol: "Be",
+      r: 2,
+      c: 2,
+      type: "AlkalineEarth",
+      number: 4,
+      weight: 9.012
+    },
+    {
+      name: "Boron",
+      symbol: "B",
+      r: 2,
+      c: 13,
+      type: "Metalloid",
+      number: 5,
+      weight: 10.811
+    },
+    {
+      name: "Carbon",
+      symbol: "C",
+      r: 2,
+      c: 14,
+      type: "NonMetal",
+      number: 6,
+      weight: 12.011
+    },
+    {
+      name: "Nitrogen",
+      symbol: "N",
+      r: 2,
+      c: 15,
+      type: "NonMetal",
+      number: 7,
+      weight: 14.007
+    },
+    {
+      name: "Oxygen",
+      symbol: "O",
+      r: 2,
+      c: 16,
+      type: "NonMetal",
+      number: 8,
+      weight: 15.999
+    },
+    {
+      name: "Fluorine",
+      symbol: "F",
+      r: 2,
+      c: 17,
+      type: "NonMetal",
+      number: 9,
+      weight: 18.998
+    },
+    {
+      name: "Neon",
+      symbol: "Ne",
+      r: 2,
+      c: 18,
+      type: "NobleGas",
+      number: 10,
+      weight: 20.18
+    },
+    {
+      name: "Sodium",
+      symbol: "Na",
+      r: 3,
+      c: 1,
+      type: "AlkaliMetal",
+      number: 11,
+      weight: 22.99
+    },
+    {
+      name: "Magnesium",
+      symbol: "Mg",
+      r: 3,
+      c: 2,
+      type: "AlkalineEarth",
+      number: 12,
+      weight: 24.305
+    },
+    {
+      name: "Aluminum",
+      symbol: "Al",
+      r: 3,
+      c: 13,
+      type: "PostTransitionMetal",
+      number: 13,
+      weight: 26.982
+    },
+    {
+      name: "Silicon",
+      symbol: "Si",
+      r: 3,
+      c: 14,
+      type: "Metalloid",
+      number: 14,
+      weight: 28.085
+    },
+    {
+      name: "Phosphorus",
+      symbol: "P",
+      r: 3,
+      c: 15,
+      type: "NonMetal",
+      number: 15,
+      weight: 30.974
+    },
+    {
+      name: "Sulfur",
+      symbol: "S",
+      r: 3,
+      c: 16,
+      type: "NonMetal",
+      number: 16,
+      weight: 32.06
+    },
+    {
+      name: "Chlorine",
+      symbol: "Cl",
+      r: 3,
+      c: 17,
+      type: "NonMetal",
+      number: 17,
+      weight: 35.45
+    },
+    {
+      name: "Argon",
+      symbol: "Ar",
+      r: 3,
+      c: 18,
+      type: "NobleGas",
+      number: 18,
+      weight: 39.948
+    },
+    {
+      name: "Potassium",
+      symbol: "K",
+      r: 4,
+      c: 1,
+      type: "AlkaliMetal",
+      number: 19,
+      weight: 39.098
+    },
+    {
+      name: "Calcium",
+      symbol: "Ca",
+      r: 4,
+      c: 2,
+      type: "AlkalineEarth",
+      number: 20,
+      weight: 40.078
+    },
+    {
+      name: "Scandium",
+      symbol: "Sc",
+      r: 4,
+      c: 3,
+      type: "TransitionMetal",
+      number: 21,
+      weight: 44.956
+    },
+    {
+      name: "Titanium",
+      symbol: "Ti",
+      r: 4,
+      c: 4,
+      type: "TransitionMetal",
+      number: 22,
+      weight: 47.867
+    },
+    {
+      name: "Vanadium",
+      symbol: "V",
+      r: 4,
+      c: 5,
+      type: "TransitionMetal",
+      number: 23,
+      weight: 50.942
+    },
+    {
+      name: "Chromium",
+      symbol: "Cr",
+      r: 4,
+      c: 6,
+      type: "TransitionMetal",
+      number: 24,
+      weight: 51.996
+    },
+    {
+      name: "Manganese",
+      symbol: "Mn",
+      r: 4,
+      c: 7,
+      type: "TransitionMetal",
+      number: 25,
+      weight: 54.938
+    },
+    {
+      name: "Iron",
+      symbol: "Fe",
+      r: 4,
+      c: 8,
+      type: "TransitionMetal",
+      number: 26,
+      weight: 55.845
+    },
+    {
+      name: "Cobalt",
+      symbol: "Co",
+      r: 4,
+      c: 9,
+      type: "TransitionMetal",
+      number: 27,
+      weight: 58.933
+    },
+    {
+      name: "Nickel",
+      symbol: "Ni",
+      r: 4,
+      c: 10,
+      type: "TransitionMetal",
+      number: 28,
+      weight: 58.693
+    },
+    {
+      name: "Copper",
+      symbol: "Cu",
+      r: 4,
+      c: 11,
+      type: "TransitionMetal",
+      number: 29,
+      weight: 63.546
+    },
+    {
+      name: "Zinc",
+      symbol: "Zn",
+      r: 4,
+      c: 12,
+      type: "TransitionMetal",
+      number: 30,
+      weight: 65.38
+    },
+    {
+      name: "Gallium",
+      symbol: "Ga",
+      r: 4,
+      c: 13,
+      type: "PostTransitionMetal",
+      number: 31,
+      weight: 69.723
+    },
+    {
+      name: "Germanium",
+      symbol: "Ge",
+      r: 4,
+      c: 14,
+      type: "Metalloid",
+      number: 32,
+      weight: 72.63
+    },
+    {
+      name: "Arsenic",
+      symbol: "As",
+      r: 4,
+      c: 15,
+      type: "Metalloid",
+      number: 33,
+      weight: 74.922
+    },
+    {
+      name: "Selenium",
+      symbol: "Se",
+      r: 4,
+      c: 16,
+      type: "NonMetal",
+      number: 34,
+      weight: 78.971
+    },
+    {
+      name: "Bromine",
+      symbol: "Br",
+      r: 4,
+      c: 17,
+      type: "NonMetal",
+      number: 35,
+      weight: 79.904
+    },
+    {
+      name: "Krypton",
+      symbol: "Kr",
+      r: 4,
+      c: 18,
+      type: "NobleGas",
+      number: 36,
+      weight: 83.798
+    },
+    {
+      name: "Rubidium",
+      symbol: "Rb",
+      r: 5,
+      c: 1,
+      type: "AlkaliMetal",
+      number: 37,
+      weight: 85.468
+    },
+    {
+      name: "Strontium",
+      symbol: "Sr",
+      r: 5,
+      c: 2,
+      type: "AlkalineEarth",
+      number: 38,
+      weight: 87.62
+    },
+    {
+      name: "Yttrium",
+      symbol: "Y",
+      r: 5,
+      c: 3,
+      type: "TransitionMetal",
+      number: 39,
+      weight: 88.906
+    },
+    {
+      name: "Zirconium",
+      symbol: "Zr",
+      r: 5,
+      c: 4,
+      type: "TransitionMetal",
+      number: 40,
+      weight: 91.224
+    },
+    {
+      name: "Niobium",
+      symbol: "Nb",
+      r: 5,
+      c: 5,
+      type: "TransitionMetal",
+      number: 41,
+      weight: 92.906
+    },
+    {
+      name: "Molybdenur",
+      symbol: "Mo",
+      r: 5,
+      c: 6,
+      type: "TransitionMetal",
+      number: 42,
+      weight: 95.95
+    },
+    {
+      name: "Technetium",
+      symbol: "Tc",
+      r: 5,
+      c: 7,
+      type: "TransitionMetal",
+      number: 43,
+      weight: 98
+    },
+    {
+      name: "Ruthenium",
+      symbol: "Ru",
+      r: 5,
+      c: 8,
+      type: "TransitionMetal",
+      number: 44,
+      weight: 101.07
+    },
+    {
+      name: "Rhodium",
+      symbol: "Rh",
+      r: 5,
+      c: 9,
+      type: "TransitionMetal",
+      number: 45,
+      weight: 102.91
+    },
+    {
+      name: "Palladium",
+      symbol: "Pd",
+      r: 5,
+      c: 10,
+      type: "TransitionMetal",
+      number: 46,
+      weight: 106.42
+    },
+    {
+      name: "Silver",
+      symbol: "Ag",
+      r: 5,
+      c: 11,
+      type: "TransitionMetal",
+      number: 47,
+      weight: 107.87
+    },
+    {
+      name: "Cadmium",
+      symbol: "Cd",
+      r: 5,
+      c: 12,
+      type: "TransitionMetal",
+      number: 48,
+      weight: 112.41
+    },
+    {
+      name: "Indium",
+      symbol: "In",
+      r: 5,
+      c: 13,
+      type: "PostTransitionMetal",
+      number: 49,
+      weight: 114.82
+    },
+    {
+      name: "Tin",
+      symbol: "Sn",
+      r: 5,
+      c: 14,
+      type: "PostTransitionMetal",
+      number: 50,
+      weight: 118.71
+    },
+    {
+      name: "Antimony",
+      symbol: "Sb",
+      r: 5,
+      c: 15,
+      type: "Metalloid",
+      number: 51,
+      weight: 121.76
+    },
+    {
+      name: "Tellurium",
+      symbol: "Te",
+      r: 5,
+      c: 16,
+      type: "Metalloid",
+      number: 52,
+      weight: 127.6
+    },
+    {
+      name: "Iodine",
+      symbol: "I",
+      r: 5,
+      c: 17,
+      type: "NonMetal",
+      number: 53,
+      weight: 126.9
+    },
+    {
+      name: "Xenon",
+      symbol: "Xe",
+      r: 5,
+      c: 18,
+      type: "NobleGas",
+      number: 54,
+      weight: 131.29
+    },
+    {
+      name: "Caesium",
+      symbol: "Cs",
+      r: 6,
+      c: 1,
+      type: "AlkaliMetal",
+      number: 55,
+      weight: 132.91
+    },
+    {
+      name: "Barium",
+      symbol: "Ba",
+      r: 6,
+      c: 2,
+      type: "AlkalineEarth",
+      number: 56,
+      weight: 137.33
+    },
+    {
+      name: "",
+      symbol: "57-71",
+      r: 6,
+      c: 3,
+      type: "Lanthanoid",
+      number: null,
+      weight: null
+    },
+    {
+      name: "Hafnium",
+      symbol: "Hf",
+      r: 6,
+      c: 4,
+      type: "TransitionMetal",
+      number: 72,
+      weight: 178.49
+    },
+    {
+      name: "Tantalum",
+      symbol: "Ta",
+      r: 6,
+      c: 5,
+      type: "TransitionMetal",
+      number: 73,
+      weight: 180.95
+    },
+    {
+      name: "Tungsten",
+      symbol: "W",
+      r: 6,
+      c: 6,
+      type: "TransitionMetal",
+      number: 74,
+      weight: 183.84
+    },
+    {
+      name: "Rhenium",
+      symbol: "Re",
+      r: 6,
+      c: 7,
+      type: "TransitionMetal",
+      number: 75,
+      weight: 186.21
+    },
+    {
+      name: "Osmium",
+      symbol: "Os",
+      r: 6,
+      c: 8,
+      type: "TransitionMetal",
+      number: 76,
+      weight: 190.23
+    },
+    {
+      name: "Iridium",
+      symbol: "Ir",
+      r: 6,
+      c: 9,
+      type: "TransitionMetal",
+      number: 77,
+      weight: 192.22
+    },
+    {
+      name: "Platinum",
+      symbol: "Pt",
+      r: 6,
+      c: 10,
+      type: "TransitionMetal",
+      number: 78,
+      weight: 195.08
+    },
+    {
+      name: "Gold",
+      symbol: "Au",
+      r: 6,
+      c: 11,
+      type: "TransitionMetal",
+      number: 79,
+      weight: 196.97
+    },
+    {
+      name: "Mercury",
+      symbol: "Hg",
+      r: 6,
+      c: 12,
+      type: "TransitionMetal",
+      number: 80,
+      weight: 200.59
+    },
+    {
+      name: "Thallium",
+      symbol: "Tl",
+      r: 6,
+      c: 13,
+      type: "PostTransitionMetal",
+      number: 81,
+      weight: 204.38
+    },
+    {
+      name: "Lead",
+      symbol: "Pb",
+      r: 6,
+      c: 14,
+      type: "PostTransitionMetal",
+      number: 82,
+      weight: 207.2
+    },
+    {
+      name: "Bismuth",
+      symbol: "Bi",
+      r: 6,
+      c: 15,
+      type: "PostTransitionMetal",
+      number: 83,
+      weight: 208.98
+    },
+    {
+      name: "Polonium",
+      symbol: "Po",
+      r: 6,
+      c: 16,
+      type: "TransitionMetal",
+      number: 84,
+      weight: 209
+    },
+    {
+      name: "Astatine",
+      symbol: "At",
+      r: 6,
+      c: 17,
+      type: "Metalloid",
+      number: 85,
+      weight: 210
+    },
+    {
+      name: "Radon",
+      symbol: "Rn",
+      r: 6,
+      c: 18,
+      type: "NobleGas",
+      number: 86,
+      weight: 222
+    },
+    {
+      name: "Francium",
+      symbol: "Fr",
+      r: 7,
+      c: 1,
+      type: "AlkaliMetal",
+      number: 87,
+      weight: 223
+    },
+    {
+      name: "Radium",
+      symbol: "Ra",
+      r: 7,
+      c: 2,
+      type: "AlkalineEarth",
+      number: 88,
+      weight: 226
+    },
+    {
+      name: "",
+      symbol: "89-103",
+      r: 7,
+      c: 3,
+      type: "Actinoid",
+      weight: null
+    },
+    {
+      name: "Rutherfordium",
+      symbol: "Rf",
+      r: 7,
+      c: 4,
+      type: "TransitionMetal",
+      number: 104,
+      weight: 267
+    },
+    {
+      name: "Dubnium",
+      symbol: "Db",
+      r: 7,
+      c: 5,
+      type: "TransitionMetal",
+      number: 105,
+      weight: 268
+    },
+    {
+      name: "Seaborgium",
+      symbol: "Sg",
+      r: 7,
+      c: 6,
+      type: "TransitionMetal",
+      number: 106,
+      weight: 269
+    },
+    {
+      name: "Bohrium",
+      symbol: "Bh",
+      r: 7,
+      c: 7,
+      type: "TransitionMetal",
+      number: 107,
+      weight: 270
+    },
+    {
+      name: "Hassium",
+      symbol: "Hs",
+      r: 7,
+      c: 8,
+      type: "TransitionMetal",
+      number: 108,
+      weight: 270
+    },
+    {
+      name: "Meitnerium",
+      symbol: "Mt",
+      r: 7,
+      c: 9,
+      type: "Other",
+      number: 109,
+      weight: 278
+    },
+    {
+      name: "Darmstadtium",
+      symbol: "Ds",
+      r: 7,
+      c: 10,
+      type: "Other",
+      number: 110,
+      weight: 281
+    },
+    {
+      name: "Roentgenium",
+      symbol: "Rg",
+      r: 7,
+      c: 11,
+      type: "Other",
+      number: 111,
+      weight: 282
+    },
+    {
+      name: "Copernicium",
+      symbol: "Cn",
+      r: 7,
+      c: 12,
+      type: "TransitionMetal",
+      number: 112,
+      weight: 285
+    },
+    {
+      name: "Nihonium",
+      symbol: "Nh",
+      r: 7,
+      c: 13,
+      type: "Other",
+      number: 113,
+      weight: 286
+    },
+    {
+      name: "Flerovium",
+      symbol: "Fl",
+      r: 7,
+      c: 14,
+      type: "PostTransitionMetal",
+      number: 114,
+      weight: 289
+    },
+    {
+      name: "Moscovium",
+      symbol: "Mc",
+      r: 7,
+      c: 15,
+      type: "Other",
+      number: 115,
+      weight: 290
+    },
+    {
+      name: "Livermorium",
+      symbol: "Lv",
+      r: 7,
+      c: 16,
+      type: "Other",
+      number: 116,
+      weight: 293
+    },
+    {
+      name: "Tennessine",
+      symbol: "Ts",
+      r: 7,
+      c: 17,
+      type: "Other",
+      number: 117,
+      weight: 294
+    },
+    {
+      name: "Oganesson",
+      symbol: "Og",
+      r: 7,
+      c: 18,
+      type: "Other",
+      number: 118,
+      weight: 294
+    },
+    {
+      name: "Lanthanum",
+      symbol: "La",
+      r: 8,
+      c: 4,
+      type: "Lanthanoid",
+      number: 57,
+      weight: 138.91
+    },
+    {
+      name: "Cerium",
+      symbol: "Ce",
+      r: 8,
+      c: 5,
+      type: "Lanthanoid",
+      number: 58,
+      weight: 140.12
+    },
+    {
+      name: "Praseodynium",
+      symbol: "Pr",
+      r: 8,
+      c: 6,
+      type: "Lanthanoid",
+      number: 59,
+      weight: 140.91
+    },
+    {
+      name: "Neodymium",
+      symbol: "Nd",
+      r: 8,
+      c: 7,
+      type: "Lanthanoid",
+      number: 60,
+      weight: 144.24
+    },
+    {
+      name: "Promethium",
+      symbol: "Pm",
+      r: 8,
+      c: 8,
+      type: "Lanthanoid",
+      number: 61,
+      weight: 145
+    },
+    {
+      name: "Samarium",
+      symbol: "Sm",
+      r: 8,
+      c: 9,
+      type: "Lanthanoid",
+      number: 62,
+      weight: 150.36
+    },
+    {
+      name: "Europium",
+      symbol: "Eu",
+      r: 8,
+      c: 10,
+      type: "Lanthanoid",
+      number: 63,
+      weight: 151.96
+    },
+    {
+      name: "Gadolinium",
+      symbol: "Gd",
+      r: 8,
+      c: 11,
+      type: "Lanthanoid",
+      number: 64,
+      weight: 157.25
+    },
+    {
+      name: "Terbium",
+      symbol: "Tb",
+      r: 8,
+      c: 12,
+      type: "Lanthanoid",
+      number: 65,
+      weight: 158.93
+    },
+    {
+      name: "Dysprosium",
+      symbol: "Dy",
+      r: 8,
+      c: 13,
+      type: "Lanthanoid",
+      number: 66,
+      weight: 162.5
+    },
+    {
+      name: "Holmium",
+      symbol: "Ho",
+      r: 8,
+      c: 14,
+      type: "Lanthanoid",
+      number: 67,
+      weight: 164.93
+    },
+    {
+      name: "Erbium",
+      symbol: "Er",
+      r: 8,
+      c: 15,
+      type: "Lanthanoid",
+      number: 68,
+      weight: 167.26
+    },
+    {
+      name: "Thulium",
+      symbol: "Tm",
+      r: 8,
+      c: 16,
+      type: "Lanthanoid",
+      number: 69,
+      weight: 168.93
+    },
+    {
+      name: "Ytterbium",
+      symbol: "Yb",
+      r: 8,
+      c: 17,
+      type: "Lanthanoid",
+      number: 70,
+      weight: 173.05
+    },
+    {
+      name: "Lutetium",
+      symbol: "Lu",
+      r: 8,
+      c: 18,
+      type: "Lanthanoid",
+      number: 71,
+      weight: 174.97
+    },
+    {
+      name: "Actinium",
+      symbol: "Ac",
+      r: 9,
+      c: 4,
+      type: "Actinoid",
+      number: 89,
+      weight: 227
+    },
+    {
+      name: "Thorium",
+      symbol: "Th",
+      r: 9,
+      c: 5,
+      type: "Actinoid",
+      number: 90,
+      weight: 232.04
+    },
+    {
+      name: "Protactinium",
+      symbol: "Pa",
+      r: 9,
+      c: 6,
+      type: "Actinoid",
+      number: 91,
+      weight: 231.04
+    },
+    {
+      name: "Uranium",
+      symbol: "U",
+      r: 9,
+      c: 7,
+      type: "Actinoid",
+      number: 92,
+      weight: 238.03
+    },
+    {
+      name: "Neptunium",
+      symbol: "Np",
+      r: 9,
+      c: 8,
+      type: "Actinoid",
+      number: 93,
+      weight: 237
+    },
+    {
+      name: "Plutonium",
+      symbol: "Pu",
+      r: 9,
+      c: 9,
+      type: "Actinoid",
+      number: 94,
+      weight: 244
+    },
+    {
+      name: "Americium",
+      symbol: "Am",
+      r: 9,
+      c: 10,
+      type: "Actinoid",
+      number: 95,
+      weight: 243
+    },
+    {
+      name: "Curium",
+      symbol: "Cm",
+      r: 9,
+      c: 11,
+      type: "Actinoid",
+      number: 96,
+      weight: 247
+    },
+    {
+      name: "Berkelium",
+      symbol: "Bk",
+      r: 9,
+      c: 12,
+      type: "Actinoid",
+      number: 97,
+      weight: 247
+    },
+    {
+      name: "Californium",
+      symbol: "Cf",
+      r: 9,
+      c: 13,
+      type: "Actinoid",
+      number: 98,
+      weight: 251
+    },
+    {
+      name: "Einsteinium",
+      symbol: "Es",
+      r: 9,
+      c: 14,
+      type: "Actinoid",
+      number: 99,
+      weight: 252
+    },
+    {
+      name: "Fermium",
+      symbol: "Fm",
+      r: 9,
+      c: 15,
+      type: "Actinoid",
+      number: 100,
+      weight: 257
+    },
+    {
+      name: "Mendelvium",
+      symbol: "Md",
+      r: 9,
+      c: 16,
+      type: "Actinoid",
+      number: 101,
+      weight: 258
+    },
+    {
+      name: "Nobelium",
+      symbol: "No",
+      r: 9,
+      c: 17,
+      type: "Actinoid",
+      number: 102,
+      weight: 259
+    },
+    {
+      name: "Lawrencium",
+      symbol: "Lr",
+      r: 9,
+      c: 18,
+      type: "Actinoid",
+      number: 103,
+      weight: 266
+    }
+  ];
 }
